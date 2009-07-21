744fff8c2d350ef9e392455e8c81411fdf381c90
tymofiy <tymofi@me.com>
2009-07-20 22:38:51 -0400

    Add Ukrainian language translation

---
+++ frameworks/shared_assets/uk.lproj/strings.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Project:   Website Strings
+// Copyright: ©2009 Sprout Systems, Inc.
+// ==========================================================================
+/*globals Home */
+
+// Place strings you want to localize here.  In your app, use the key and
+// localize it using "key string".loc().  HINT: For your key names, use the
+// english string with an underscore in front.  This way you can still see
+// how your UI will look and you'll notice right away when something needs a
+// localized string added to this file!
+//
+SC.stringsFor('Українська', {
+  "_Home": "Головна",
+  "_Download": "Завантажити",
+  "_Wiki": "Вікі",
+  "_Screencasts": "Відео",
+  "_Blog": "Блог",
+  "_Footer Copyright": "© 2008-2009, Sprout Systems, Inc. Всі права захищені.",
+  "_Problem with the site?": "Знайдені проблеми з сайтом? Немає проблеми.",
+  "_Its Open Source!": "Вихідний код відкрит!"
+}) ;
---
+++ pages/home/uk.lproj/strings.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   Home Strings
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Home */
+
+// Place strings you want to localize here.  In your app, use the key and
+// localize it using "key string".loc().  HINT: For your key names, use the
+// english string with an underscore in front.  This way you can still see
+// how your UI will look and you'll notice right away when something needs a
+// localized string added to this file!
+//
+SC.stringsFor('Українська', {
+  "_Introducing SproutCore 1.0": "Представляємо SproutCore 1.0",
+  "_HTML5 Application Framework": "Програмний фреймворк HTML5",
+  "_Lead.Headline": "Постачання приголомшливих настільних додатків в <em>будь-якому сучасному веб-браузері</em> без плагінів.",
+  
+  "_Beta.Headline": "SproutCore 1.0 — <strong>Бета-версія!</strong>",
+  "_Beta.Body": "Після 20000 строк нового коду, більше 5000 нових юніт-тестів, і незліченну кількість годин роботи понад 30 вкладників, SproutCore 1.0 майже готовий. Спробуйте новий код сьогодні, і допоможіть нам зробити поштовх для нашої остаточної версії. Приєднуйтесь до революції HTML5.",
+  
+  "_Learn More": "Докладніше",
+  "_Latest News": "Останні новини",
+  "_Get Started": "Почніть тут"
+}) ;
