ed4579e4e1ea0a91ab2a04befd327dbcb86892ae
Onar Vikingstad <onar@Onars-MacBook-Pro.local>
2009-07-21 19:57:26 -0700

    Norwegian strings of home

---
+++ pages/home/no.lproj/strings.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   Home Strings
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Home */
+
+// Place strings you want to localize here.  In your app, use the key and
+// localize it using "key string".loc().  HINT: For your key names, use the
+// english string with an underscore in front.  This way you can still see
+// how your UI will look and you'll notice right away when something needs a
+// localized string added to this file!
+//
+SC.stringsFor('Norsk (Bokmål)', {
+  "_Introducing SproutCore 1.0": "Introduserer SproutCore 1.0",
+  "_HTML5 Application Framework": "HTML5 Programmeringsrammeverk",
+  "_Lead.Headline": "Lag imponerende desktop lignende web applikasjoner i <em>hvilken som helst nettleser</em> uten plugins.",
+  
+  "_Beta.Headline": "SproutCore 1.0 er <strong>Beta!</strong>",
+  "_Beta.Body": "Etter 20 000 kodelinjer, mer enn 5 000 nye unit tester, og utallige timer fra mer enn 30 bidragsytere, SproutCore 1.0 er nesten ferdig. Prøv den nye versjonen i dag og hjelp oss me å komme i mål med final release. Kom bli med på HTML5 revolusjonen!",
+  
+  "_Learn More": "Lær mer",
+  "_Latest News": "Siste nytt",
+  "_Get Started": "Kom igang"
+}) ;
