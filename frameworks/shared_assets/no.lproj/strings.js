d110761b403e1e38953034e0092ba1c4da84f2cc
Onar Vikingstad <onar@Onars-MacBook-Pro.local>
2009-07-21 19:48:37 -0700

    Norwegian strings

---
+++ frameworks/shared_assets/no.lproj/strings.js
@@ -0,0 +1,25 @@
+// ==========================================================================
+// Project:   Website Strings
+// Copyright: ©2009 Sprout Systems, Inc.
+// ==========================================================================
+/*globals Home */
+
+// Place strings you want to localize here.  In your app, use the key and
+// localize it using "key string".loc().  HINT: For your key names, use the
+// english string with an underscore in front.  This way you can still see
+// how your UI will look and you'll notice right away when something needs a
+// localized string added to this file!
+//
+SC.stringsFor('Norsk (Bokmål)', {
+  "_Home": "Hjem",
+  "_Download": "Nedlasting",
+  "_Wiki": "Wiki",
+  "_Screencasts": "Screencasts",
+  "_Blog": "Blogg",
+  "_Footer Copyright": "copyright 2008-2009, Sprout Systems, Inc. Alle rettigheter forbeholdes.",
+  "_Problem with the site?": "Funnet et problem med siden? Ikke noe problem.",
+  "_Its Open Source!": "Den er open source!",
+  
+  "_Select a language:": "Velg et språk:",
+  "_Choose Language": "Velg språk"
+}) ;
